70d5e4388fa88f59cd525366118b7674d2e83f1c
Bussyadex <Bussyadex42@gmail.com>
2024-01-18 09:55:02 +0000

    InvalidPurchaseFix

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -26,7 +26,9 @@ public class TicketServiceImpl implements TicketService {
 
         int totalAmountPaymentDue = 0;
         int totalSeatsAllocationDue = 0;
-        boolean adultIsPresent = false;
+        int totalTicketCount = 0;
+        int infantTicket = 0;
+        int adultTicket = 0;
 
         for (TicketTypeRequest ticket : ticketTypeRequests) {
             Type typeOfTicket = ticket.getTicketType();
@@ -34,38 +36,39 @@ public class TicketServiceImpl implements TicketService {
 
             if (typeOfTicket == Type.INFANT){
                 checkTicketNumberRange(noOfTicket, "Infant");
+                infantTicket += noOfTicket;
+                totalTicketCount += noOfTicket;
             }
             else if (typeOfTicket == Type.CHILD){
                 if (checkTicketNumberRange(noOfTicket, "Child") && noOfTicket > 0){
                     totalAmountPaymentDue += (noOfTicket * 10);
                     totalSeatsAllocationDue += noOfTicket;
+                    totalTicketCount += noOfTicket;
                 }
             }
             else if (typeOfTicket == Type.ADULT) {
                 if (checkTicketNumberRange(noOfTicket, "Adult") && noOfTicket > 0){
                     totalAmountPaymentDue += (noOfTicket * 20);
                     totalSeatsAllocationDue += noOfTicket;
-                    adultIsPresent = true;
+                    adultTicket += noOfTicket;
                 }
             } else {
                 throw new InvalidPurchaseException("Invalid ticket - Undefined..");
             }
         }
 
-        checkTicketNumberRange(totalSeatsAllocationDue, "total");
+        checkTicketNumberRange(totalTicketCount, "total");
 
         if (totalSeatsAllocationDue < 1) {
             throw new InvalidPurchaseException("No Valid Ticket.. Number of ticket should be atleast 1");
         }
 
-        if(adultIsPresent){
+        if(checkValidPurchase (adultTicket, infantTicket)){
             _paymentService.makePayment(accountId, totalAmountPaymentDue);
-            _reservationService.reserveSeat(accountId, totalSeatsAllocationDue);    
-                
-        }else{
-            throw new InvalidPurchaseException("Infact or Child tickets are required to have atleast one adult");
+            _reservationService.reserveSeat(accountId, totalSeatsAllocationDue);
         }
-    }
+        
+        }
 
     private boolean checkTicketNumberRange(int noOfTicket, String name) {
         if (noOfTicket > 20) {
@@ -77,4 +80,13 @@ public class TicketServiceImpl implements TicketService {
         return true;
     }
     
+    private boolean checkValidPurchase (int adultTicket, int infantTicket){
+        if (adultTicket < 1){
+            throw new InvalidPurchaseException("Infact or Child tickets are required to have atleast one adult"); 
+        }
+        if (infantTicket > (adultTicket/2)) {
+            throw new InvalidPurchaseException("Number of Infant is more than Adult available .. A maximum of 2 Infant per Adult");
+        }
+        return true;
+    }
 }
